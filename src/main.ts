4f2a14a20184ecf0c435368081ec774ec21304ba
Geoff Gaudreault <neurofuzzy@users.noreply.github.com>
2024-10-27 15:46:44 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -42,18 +42,19 @@ const segs = 512;
 const scale = 0.45;
 const minBand = 2;
 const logPadding = 0.7;
+const doSpiral = true;
 
 let animate = false;
 
 const func = (perc: number) => {
-  //const s = stepNum; // ring
-  const s = stepNum + perc; // spiral
+  const s = doSpiral ? stepNum + perc : stepNum;
+  const a = amp * Math.log2(s / bands + 1);
   const twist = s / 50;
   let pt = new Point(0, 0);
   pt.x = 0;
   pt.y = Math.log2(s + logPadding) * size; // spiral
-  pt.x -= Math.sin(perc * Math.PI * 3 * freq) * s * amp;
-  pt.y += Math.cos(perc * Math.PI * 3 * freq) * s * amp;
+  pt.x -= Math.sin(perc * Math.PI * 3 * freq) * s * a;
+  pt.y += Math.cos(perc * Math.PI * 3 * freq) * s * a;
 
   GeomHelpers.rotatePoint(pt, perc * Math.PI * 2 + twist);
   return pt;
@@ -64,22 +65,22 @@ const func = (perc: number) => {
 function getPaths() {
   Sequence.fromStatement("random 0-10 AS XX", 288);
 
-  let polygons: Path[] = [];
+  let paths: Path[] = [];
   let step = 4;
   let r = step * 10;
 
   let bones = [];
 
   for (let x = 0; x < bands; x++) {
-    const poly = new ParametricPath(func, segs);
+    const path = new ParametricPath(func, segs);
     stepNum = x;
-    let pts: Point[] = poly.toPoints();
+    let pts: Point[] = path.toPoints();
     pts.forEach((pt) => {
       pt.x += w / 2;
       pt.y += h / 2;
     });
-    let poly2 = new Path(pts);
-    bones.push(poly2);
+    let path2 = new Path(pts);
+    bones.push(path2);
     r += step * 4;
   }
 
@@ -99,14 +100,24 @@ function getPaths() {
         pt.y += h / 2;
         pts.push(pt);
       });
-      const poly = new Path(pts);
+      const path = new Path(pts);
       if (x >= minBand) {
-        polygons.push(poly);
+        paths.push(path);
       }
     }
   }
 
-  return polygons;
+  if (doSpiral) {
+    // join all paths points into one
+    let pts: Point[] = [];
+    paths.forEach((path) => {
+      pts = pts.concat(path.points);
+    });
+    const path = new Path(pts);
+    paths = [path];
+  }
+
+  return paths;
 }
 
 const draw = (ctx: CanvasRenderingContext2D) => {
