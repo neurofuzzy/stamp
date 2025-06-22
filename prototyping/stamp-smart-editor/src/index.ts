94f4df429df0bdd26498ce81a3897b2c47f1d647
Geoff Gaudreault <neurofuzzy@users.noreply.github.com>
2025-06-22 13:45:02 -0700

    cleanup

--- prototyping/stamp-smart-editor/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { StampTokenizer, TokenType } from './stamp-tokenizer';
-export { StampAutocomplete, SuggestionType } from './stamp-autocomplete';
-export type { Token, CursorContext } from './stamp-tokenizer';
-export type { Suggestion } from './stamp-autocomplete'; 
