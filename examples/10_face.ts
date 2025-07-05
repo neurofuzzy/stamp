4c81779191aa3ef97e4c51bfcfaa212fc897fbdf
Geoff Gaudreault <neurofuzzy@users.noreply.github.com>
2025-07-05 14:50:02 -0700

    Update 10_face.ts

--- examples/10_face.ts
+++ examples/10_face.ts
@@ -30,42 +30,62 @@ function draw(ctx: CanvasRenderingContext2D) {
 
   // Compose the face using Stamp's composition API
   const face = new Stamp(new Ray(w/2, h/2, 0))
-  .defaultStyle({
-    // fillColor: "COLOR()",
-    strokeThickness: 1,
-    fillColor: 0,
-  })
     // Head
-    .circle({ radius: 120 })
+    .rectangle({ width: 250, height: 250, align: ShapeAlignment.CENTER })
     // Left eye
     .subtract()
     .moveTo(-45, -40)
-    .circle({ radius: 18 })
-    .circle({ radius: 7 })
+    .rectangle({ width: 48, height: 48 })
+    .add()
+    .rectangle({ width: 20, height: 20 })
     // Right eye
     .subtract()
     .moveTo(45, -40)
-    .circle({ radius: 18 })
-    .circle({ radius: 7 })
+    .rectangle({ width: 48, height: 48 })
+    .add()
+    .rectangle({ width: 20, height: 20 })
     // Smile (simple arc/ellipse for mouth)
     .subtract()
     .moveTo(0, 50)
-    .rectangle({ width: 60, height: 28, align: ShapeAlignment.BOTTOM })
+    .rectangle({ width: 140, height: 20, align: ShapeAlignment.BOTTOM })
+    .rotate(90)
+    .forward(70)
+    .rectangle({ width: 40, height: 20, align: ShapeAlignment.BOTTOM })
+    .rotate(180)
+    .forward(140)
+    .rectangle({ width: 40, height: 20, align: ShapeAlignment.BOTTOM })
     
-  
-  face.children().forEach(child => drawShape(ctx, child));
+  drawShape(ctx, face, 0);
 }
 
+document.onkeydown = function (e) {
+  // if enter
+  if (e.keyCode === 13) {
+    // reset Sequences
+    Sequence.resetAll();
+    // export the canvas as SVG
+    const ctx2 = new C2S(canvas.width / ratio, canvas.height / ratio);
+    // draw the boundary
+    ctx2.backgroundColor = "#000";
+    // draw the shapes
+    draw(ctx2);
+    // download the SVG
+    const svg = ctx2.getSerializedSvg(false).split("#FFFFFF").join("#000000");
+    const svgNoBackground = svg.replace(/\<rect.*?\>/g, "");
+    const blob = new Blob([svgNoBackground], { type: "image/svg+xml" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = `stamp-${new Date().toISOString()}.svg`;
+    link.click();
+  }
+};
 
 async function main() {
-  
   await ClipperHelpers.init();
 
   const now = new Date().getTime();
   draw(ctx);
   console.log(`${new Date().getTime() - now}ms`);
-
 }
-
 
 main();
