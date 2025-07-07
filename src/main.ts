fbbb6ab8d484496e8ce72428dcc6e83f31bd975d
Geoff Gaudreault <neurofuzzy@users.noreply.github.com>
2025-07-06 17:51:23 -0700

    ok

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { Ray, ShapeAlignment } from "../src/geom/core";
 import { ClipperHelpers } from '../src/lib/clipper-helpers';
 import { Stamp } from '../src/lib/stamp';
 import '../src/style.css';
+import { Sequence } from './lib/sequence';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -21,23 +22,42 @@ ctx.scale(ratio, ratio)
 const w = canvas.width / ratio;
 const h = canvas.height / ratio;
 
+Sequence.fromStatement("random 6,10,14 AS SIZE");
+
 
 function draw(ctx: CanvasRenderingContext2D) {
   ctx.clearRect(0, 0, w, h);
 
-  const distrib = new Stamp(new Ray(w/2, h/2, 0))
+  const distrib1 = new Stamp(new Ray(w/2 - 200, h/2 -200, 0))
     .circle({ 
-      radius: 10,
+      radius: 6,
       align: ShapeAlignment.CENTER,
       distribute: {
         type: "phyllotaxis",
-        count: 100,
-        scaleFactor: 20,
+        count: 70,
+        scaleFactor: 10,
         itemScaleFalloff: 1,
       }
     });
+
+  const distrib2 = new Stamp(new Ray(w/2 + 200, h/2 -200, 0))
+    .circle({ 
+      radius: "SIZE()",
+      align: ShapeAlignment.CENTER,
+      distribute: {
+        type: "attractor",
+        particleCount: 20,
+        initialRadius: 100,
+        simulationSteps: 200,
+        hexSpacing: 20,
+        strength: 1,
+        damping: 0.5,
+      }
+    });
     
-  drawShape(ctx, distrib, 0);
+    
+  drawShape(ctx, distrib1, 0);
+  drawShape(ctx, distrib2, 0);
 }
 
 async function main() {
